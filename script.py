3ebdcf8c9d4c3104dc346bdc48537a397c2065c1
Emily Yu <157428756+yuemily12@users.noreply.github.com>
2025-02-22 12:53:00 -0500

    Update script.py

--- script.py
+++ script.py
@@ -20,7 +20,10 @@ def scrape_data_point():
     Returns:
         str: The headline text if found, otherwise an empty string.
     """
-    req = requests.get("https://www.thedp.com")
+    headers = {
+        "User-Agent": "cis3500-scraper"
+    }
+    req = requests.get("https://www.thedp.com", headers=headers)
     loguru.logger.info(f"Request URL: {req.url}")
     loguru.logger.info(f"Request status code: {req.status_code}")
 
